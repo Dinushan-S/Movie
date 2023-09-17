3cf38022d9287a87507008e6b5204c9ee8d02e02
Dinushan <selvandinu18@gmail.com>
2023-09-17 23:59:23 +0530

    update

--- components/movieList.js
+++ components/movieList.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-export default function MovieList() {
+export default function MovieList({ title, data }) {
     return (
-        <View>
-            <Text>MovieList</Text>
+        <View className="mb-8 space-y-4">
+            <View className='mx-4 flex-row justify-between items-center'>
+                <Text className="text-white text-xl">{title}</Text>
+                <TouchableOpacity>
+                    <Text>See All</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
